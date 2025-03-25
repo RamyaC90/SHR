a8448091adf18910c9443d55ad396ba6cd98a5c4
RamyaC90 <Ramya90.c2@gmail.com>
2025-03-25 12:39:30 +0530

    first commit

--- src/test/java/org/sam/Output.java
+++ src/test/java/org/sam/Output.java
@@ -4,5 +4,6 @@ public class Output {
 
 	public static void main(String[] args) {
 		System.out.println("A dummy project for Maven and Github ");
+		System.out.println("I have added extra line");
 	}
 }
